ad96e939ea364e6e99c4303deb4fd1557bed57ac
Deep Patel <patel795@sheridancollege.ca>
2018-12-11 22:28:06 -0500

    Created Invoicing Controller

---
+++ Assignment3-4Api/Controllers/InvoicingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment3_4Api.Controllers
+{
+    public class InvoicingController : Controller
+    {
+        // GET: Invoicing
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
